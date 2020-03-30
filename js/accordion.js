5b7a07ee1b89802f8e46c2673b6d7ac47900a126
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2020-03-30 21:43:50 +0300

    Bad accord

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,16 @@
 $(".js-part").click(function() {
+    $(this).toggleClass('active');
     if(!$(this).hasClass('active')){
         $(this).siblings().removeClass('active');
-        $(this).addClass('active');
+        // $(this).removeClass('active');
+        // $(this).toggleClass('active');
     }
+});
+
+var elemCount = $('.product__part.active > .product__checkbox > .zrenda-checkbox').length;
+
+console.log(elemCount);
+
+$(".product__part.active .product__checkbox").css({
+    // 'height': elemCount*45+45
 });
